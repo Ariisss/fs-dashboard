79ad8e4fe7eabf7bc4bb37d7606595e91b9ae4db
Ariisss <arispantino@gmail.com>
2024-08-06 14:16:49 +0800

    feat: added url with search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string){
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if(term){
+      params.set('query', term);
+    }else{
+      params.delete('query');
+    }
+    replace(`${pathName}?${params.toString()}`);
   }
 
   return (
